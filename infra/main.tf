1d95cb09a5acdacb7ab556a1ada1364178812126
VladLesman <54897224+VladLesman@users.noreply.github.com>
2026-08-02 13:25:47 -0600

    feat: configure remote backend for tfstate and fix budget name

--- infra/main.tf
+++ infra/main.tf
@@ -10,6 +10,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+  
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate-gofunc"
+    storage_account_name = "stgofunctfstate001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
